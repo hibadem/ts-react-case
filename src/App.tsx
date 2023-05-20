2cbdb8d05c10a07d08f9be0eb9dc00b30b92bff4
halil <halilbadem47@gmail.com>
2023-05-20 15:36:52 +0300

    ts react case

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import ListItem from './components/ListItem';
+import User from './types/User';
 
-function App() {
+const App: React.FC = () => {
+  
+  const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [searchQuery, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setUsers(data);
+        setFilteredUsers(data);
+        ;
+      } catch (error) {
+        console.error('fetching users error:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleButtonClick = useCallback(() => {
+    const filteredUsers = users.filter(user => 
+      user.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredUsers(filteredUsers);
+  }, [searchQuery, users]);
+
+  const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setQuery(value);
+    if (value === '') {
+      setQuery('');
+      setFilteredUsers(users);
+    }
+  }, [users]); 
+
+  // const handleUserUpdate = (updatedUser: User) => {
+  //   setUsers((prevUsers) => {
+  //     const updatedUsers = prevUsers.map((prevUser) => {
+  //       if (prevUser.id === updatedUser.id) {
+  //         return {
+  //           ...prevUser,
+  //           name: updatedUser.name,
+  //           email: updatedUser.email
+  //         };
+  //       }
+  //       return prevUser;
+  //     });
+  //     return updatedUsers;
+  //   });
+  // };
+
+  // parametre vermezsen bu fonksiyondaki stateler update edilmeyecek eski state ele alır.
+  const handleUpdateButtonClick = useCallback((user: User) => {
+    console.log(user);
+    setUsers(users.map(prevUser => {
+      if (prevUser.id === user.id) {
+       return user;
+      }
+      return prevUser;
+    }));
+    
+  }, [users]);
+  
+  const handleDeleteButtonClick = useCallback((id: number) => {
+    setUsers(users.filter(user => user.id !== id));
+    console.log(users, id);
+    
+  }, [users]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>user list</h1>
+    <input type="search" id="site-search" name="search" value={searchQuery} onChange={handleInputChange}/>
+
+    <button onClick={handleButtonClick}>Search</button>
+    <ul>
+     {filteredUsers.map((user) => (
+      <ListItem user={user}
+       handleUpdateButtonClick={handleUpdateButtonClick}
+       handleDeleteButtonClick={handleDeleteButtonClick}/>
+     ))}
+     </ul>
+
+    <h2>users state</h2>
+    <ul>
+     {users.map((user) => {
+      return (
+        <li key={user.id}>
+          {user.name} - {user.email}
+        </li>
+      );
+     })}
+     </ul>
     </div>
   );
 }
---
+++ src/components/ListItem.tsx
@@ -0,0 +1,25 @@
+import { SetStateAction, Dispatch, useState} from "react";
+import User from "../types/User";
+
+type IProps = {
+  user: User,
+  handleUpdateButtonClick: (user: User) => void,
+  handleDeleteButtonClick: (id: number) => void
+}
+
+const ListItem: React.FC<IProps> = ({user,handleUpdateButtonClick, handleDeleteButtonClick}) => {
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+
+  return (
+    <li key={user.id}>
+        <input type="text" name="name" id="user-name" value={name} onChange={(e) => setName(e.target.value)}/>
+         - 
+        <input type="email" name="email" id="user-email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <button onClick={() => handleUpdateButtonClick({id: user.id, name, email})}> Update</button>
+        <button onClick={() => handleDeleteButtonClick(user.id)}> Delete</button>
+      </li>
+  )
+}
+
+export default ListItem
